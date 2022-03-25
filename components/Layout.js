11dd347592fadee1b5e47e26f72a1f1f0c2a8835
John Olatubosun <toluolatubosun@gmail.com>
2022-03-25 12:57:19 +0100

    layout and routes

---
+++ components/Layout.js
@@ -0,0 +1,16 @@
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+    return ( 
+        <div className="font-Montserrat flex flex-col h-screen">
+            <Navbar/>
+                <div className="content flex-grow">
+                    { children }
+                </div>
+            <Footer/>
+        </div>
+    );
+}
+ 
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h1>Home</h1>
+      <h1 className="font-Montserrat">Home</h1>
     </div>
   )
 }
