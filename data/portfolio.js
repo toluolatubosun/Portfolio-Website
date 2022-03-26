bc3a7b4bd75b810542a5ecba272066fa230fe879
John Olatubosun <toluolatubosun@gmail.com>
2022-03-26 18:49:26 +0100

    portfolio work online

---
+++ data/portfolio.js
@@ -0,0 +1,11 @@
+import img_n_time from '../public/portfolio/N-Time.png'
+
+module.exports = [
+    {
+        id: 1,
+        title: "Hello World",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus auctor lectus tempor nisl aliquam lacinia.",
+        img: img_n_time,
+        url: "https://n-time.web.app"
+    }
+]
--- pages/portfolio.js
+++ pages/portfolio.js
@@ -1,15 +1,8 @@
 import Image from "next/image"
+import portfolio from '../data/portfolio'
 
-export const getStaticProps = async () => {
-    const response = await fetch(`${process.env.BASE_URL}/api/portfolio`)
-    const data = await response.json()
+const Portfolio = () => {
 
-    return {
-        props: { portfolios: data }
-    }
-}
-
-const Portfolio = ({ portfolios }) => {
     return ( 
         <div className="mt-28">
             <section>
@@ -18,19 +11,19 @@ const Portfolio = ({ portfolios }) => {
 
                 <div className="mx-8 md:mx-24 mt-10 md:mt-16">
                     {
-                        portfolios.map(portfolio => (
-                            <div className="mb-8 md:mb-20 md:grid md:grid-cols-5 md:gap-x-8" key={portfolio.id}>
+                        portfolio.map(item => (
+                            <div className="mb-8 md:mb-20 md:grid md:grid-cols-5 md:gap-x-8" key={item.id}>
                                 
                                 <div className="md:col-span-3 drop-shadow-lg">
-                                    <Image src={`/portfolio${portfolio.img}`} alt="hello" />
+                                    <Image src={item.img} alt="hello" />
                                 </div>
 
-                                <div className="py-8 md:col-span-2 md:self-end md:-mb-8">
-                                    <div className="text-primary text-2xl leading-5 mb-4">{portfolio.title}</div>
-                                    <div className="text-gray-700">{portfolio.description}</div>
+                                <div className="py-8 md:col-span-2 md:self-end md:-mb-6">
+                                    <div className="text-primary text-2xl leading-5 mb-4">{item.title}</div>
+                                    <div className="text-gray-700">{item.description}</div>
 
                                     <div className="mt-8 md:mt-6 flex justify-center md:justify-start">
-                                        <a rel="noreferrer" target="_blank" href={portfolio.url}>
+                                        <a rel="noreferrer" target="_blank" href={item.url}>
                                             <button className="bg-gradient-to-br from-primary to-blue-400  text-xl text-white font-semibold py-4 px-6 rounded inline-flex space-x-4 items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
@@ -45,24 +38,24 @@ const Portfolio = ({ portfolios }) => {
                     }
 
                     {
-                        portfolios.map(portfolio => (
-                            <div className="mb-8 md:mb-20 md:grid md:grid-cols-5 md:gap-x-20" key={portfolio.id}>
+                        portfolio.map(item => (
+                            <div className="mb-8 md:mb-20 md:grid md:grid-cols-5 md:gap-x-20" key={item.id}>
 
                                 <div className="mt-10 md:col-span-3">
                                     <div className="relative py-3">
                                         <div className="absolute inset-0 bg-gradient-to-r from-primary to-blue-400 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
                                         <div className="relative drop-shadow-lg md:rounded-lg">
-                                            <Image alt="hello" src={`/portfolio${portfolio.img}`}/>
+                                            <Image src={item.img} alt="hello"/>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div className="py-8 md:col-span-2 md:self-end md:-mb-8">
-                                    <div className="text-primary text-2xl leading-5 mb-4">{portfolio.title}</div>
-                                    <div className="text-gray-700">{portfolio.description}</div>
+                                <div className="py-8 md:col-span-2 md:self-end md:-mb-6">
+                                    <div className="text-primary text-2xl leading-5 mb-4">{item.title}</div>
+                                    <div className="text-gray-700">{item.description}</div>
 
                                     <div className="mt-8 md:mt-6 flex justify-center md:justify-start">
-                                        <a rel="noreferrer" target="_blank" href={portfolio.url}>
+                                        <a rel="noreferrer" target="_blank" href={item.url}>
                                             <button className="bg-gradient-to-br from-primary to-blue-400  text-xl text-white font-semibold py-4 px-6 rounded inline-flex space-x-4 items-center">
                                                 <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
                                                     <path strokeLinecap="round" strokeLinejoin="round" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14" />
